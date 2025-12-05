86ec458b065ce1eb2794a409f86c61eaa3d6575b
Xyrel Vaughn Delos Santos <delossantosxyrelvaughn@gmail.com>
2025-12-05 09:46:29 +0800

    Setup GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  devIndicators: false,
   output: "export",
-  basePath: "/my-dev",
-  assetPrefix: "/my-dev/",
-  images: { unoptimized: true }
+  basePath: isProd ? "/my-dev" : "",
+  assetPrefix: isProd ? "/my-dev/" : "",
+  images: {
+    unoptimized: true, // GitHub Pages requires this
+  },
 };
 
 export default nextConfig;
