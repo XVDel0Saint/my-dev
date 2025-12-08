1872a975432b3e9f66de984a673557a3d2630afd
Xyrel Vaughn Delos Santos <delossantosxyrelvaughn@gmail.com>
2025-12-08 16:24:00 +0800

    Updates

--- _next/static/_H6Sp7tsAtneNMP7SQ96X/_buildManifest.js
+++ _next/static/_H6Sp7tsAtneNMP7SQ96X/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/my-dev//_next/:path+",
-        "destination": "/my-dev/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
