7b0aaea313a17243327a7a570cace45baddc4e17
Xyrel Vaughn Delos Santos <delossantosxyrelvaughn@gmail.com>
2025-12-28 14:15:44 +0800

    removed export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   basePath: "/my-dev",
   assetPrefix: "/my-dev",
   // GitHub Pages cannot optimize images
