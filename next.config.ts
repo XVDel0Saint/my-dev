d82f67afa6736e72699af52cea780e611c566660
Xyrel Vaughn Delos Santos <delossantosxyrelvaughn@gmail.com>
2025-12-28 14:25:09 +0800

    added export mode

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   basePath: "/my-dev",
-  assetPrefix: "/my-dev",
+  assetPrefix: "/my-dev/",
   // GitHub Pages cannot optimize images
   images: {
     unoptimized: true,
