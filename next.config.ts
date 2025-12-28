570dfdb94fe671230fbcf6c14ec1944b00497d03
Xyrel Vaughn Delos Santos <delossantosxyrelvaughn@gmail.com>
2025-12-28 13:47:40 +0800

    next config changes

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,12 @@
 import type { NextConfig } from "next";
 
-const repoName = "my-dev";
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  // Required for GitHub Pages static hosting
   output: "export",
-
-  // Required because Pages serves under /my-dev
-  basePath: isProd ? `/${repoName}` : "",
-
-  // Required so CSS/JS assets load correctly
-  assetPrefix: isProd ? `/${repoName}/` : "",
-
+  basePath: "/my-dev",
+  assetPrefix: "/my-dev",
   // GitHub Pages cannot optimize images
   images: {
     unoptimized: true,
   },
 };
-
 export default nextConfig;
