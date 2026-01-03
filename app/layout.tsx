e2fa7fba0ee9d729752eab8fab31c327a081bce8
Xyrel Vaughn Delos Santos <delossantosxyrelvaughn@gmail.com>
2026-01-03 08:56:02 +0800

    Added Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,8 @@ import Script from "next/script";
 const _inter = Inter({ subsets: ["latin"] })
 const _jetbrainsMono = JetBrains_Mono({ subsets: ["latin"] })
 
+const GTM_ID = "GTM-PV7CQKN4"; // provided by Google Tag Manager
+
 export const metadata: Metadata = {
   title: "Xyrel Vaughn Delos Santos | IT Specialist & Full-Stack Developer",
   description:
@@ -39,7 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        {/* Google Analytics */}
+        {/* for Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-MRCMRWP2BH"
           strategy="afterInteractive"
@@ -53,8 +55,30 @@ export default function RootLayout({
             gtag('config', 'G-MRCMRWP2BH');
           `}
         </Script>
+
+        {/* for Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
       </head>
       <body className={`font-sans antialiased`}>
+        
+        {/* for Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         {children}
         <Analytics />
       </body>
