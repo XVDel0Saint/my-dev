d0b9a41b1991892773adc6b3abe106ab43b07923
Xyrel Vaughn Delos Santos <delossantosxyrelvaughn@gmail.com>
2026-01-29 16:53:22 +0800

    Update skills-section.tsx
    removed delay and improved transition

--- components/skills-section.tsx
+++ components/skills-section.tsx
@@ -98,7 +98,7 @@ export function SkillsSection() {
 
         <p
           className={cn(
-            "text-center mt-12 text-foreground/60 font-medium italic transition-all duration-700 delay-500",
+            "text-center mt-12 text-foreground/60 font-medium italic transition-all duration-100",
             isInView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4",
           )}
         >
